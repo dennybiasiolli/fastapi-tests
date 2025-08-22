e874b5a2c1764a2dcf89a6d1efd5aca44127ce17
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2025-08-22 18:05:57 +0200

    examples with query parameters

--- main.py
+++ main.py
@@ -13,22 +13,28 @@ class ModelName(str, Enum):
 # `docs_url=None, redoc_url=None, openapi_url=None`
 app = FastAPI()
 
+fake_items_db = [{"item_id": "Foo"}, {"item_id": "Bar"}, {"item_id": "Baz"}]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
+
 @app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    return {"user_id": user_id}
+async def read_user(user_id: int, q: str | None = None):
+    return {"user_id": user_id, "query": q}
+
 
 @app.get("/models")
 async def get_models():
     return list(ModelName)
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -42,6 +48,27 @@ async def get_model(model_name: ModelName):
         case _:
             return {"model_name": model_name, "message": "Unmapped model"}
 
+
 @app.get("/files/{file_path:path}")
 async def get_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/items/")
+async def get_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : (skip + limit)]
+
+
+@app.get("/query_params/{required_path_param}")
+async def query_params(
+    required_path_param: str,
+    required_query_param: str,
+    optional_query_param_with_default: str = "default",
+    optional_query_param: str | None = None,
+):
+    return {
+        "required_path_param": required_path_param,
+        "required_query_param": required_query_param,
+        "optional_query_param_with_default": optional_query_param_with_default,
+        "optional_query_param": optional_query_param,
+    }
